e528aea20e2c74bb9449a7f6083c5042e42a884f
Cristobal <cristobal.severino@gmail.com>
2023-02-27 09:29:04 +1100

    serialize db fix

--- src/database.ts
+++ src/database.ts
@@ -17,35 +17,35 @@ export async function createDatabase() {
 
 export async function insertRecords(db: any, weatherRecords: any[]) {
     try {
-        await db.run(`
-            CREATE TABLE IF NOT EXISTS weatherTable (
-            id TEXT PRIMARY KEY,
-            location TEXT NOT NULL,
-            temperature TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT (datetime('now', 'utc'))
-            );
-        `, function (error) {
-            if (error) {
-                console.error(`Error creating table: ${error.message}`);
-            } else {
-                console.log('Table created successfully');
+        db.serialize(() => {
+            db.run(`
+                CREATE TABLE IF NOT EXISTS weatherTable (
+                id TEXT PRIMARY KEY,
+                location TEXT NOT NULL,
+                temperature TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT (datetime('now', 'utc'))
+                );
+            `, function (error) {
+                if (error) {
+                    console.error(`Error creating table: ${error.message}`);
+                } else {
+                    console.log('Table created successfully');
+                }
+            });
+            db.run("BEGIN TRANSACTION;");
+
+            const insert = db.prepare(`
+                INSERT INTO weatherTable (id, location, temperature)
+                VALUES (?, ?, ?)
+            `);
+    
+            for (const weatherRecord of weatherRecords) {
+                insert.run(weatherRecord.id, weatherRecord.location, weatherRecord.temperature);
             }
+    
+            insert.finalize();
+            db.run("COMMIT;");
         });
-
-        await db.run("BEGIN TRANSACTION;");
-
-        const insert = await db.prepare(`
-            INSERT INTO weatherTable (id, location, temperature)
-            VALUES (?, ?, ?)
-        `);
-
-        for (const weatherRecord of weatherRecords) {
-            await insert.run(weatherRecord.id, weatherRecord.location, weatherRecord.temperature);
-        }
-
-        await insert.finalize();
-        await db.run("COMMIT;");
-        
         console.log('Records inserted successfully.');
     } catch (error) {
         console.error(`Unable to insert records: ${error}`);
